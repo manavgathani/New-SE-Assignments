a99be473c3fa1cdea8b4b8b1a30893a5dd162970
manavgathani <109840903+manavgathani@users.noreply.github.com>
2023-11-22 16:23:14 +0530

    Delete que11.c

--- que11.c
+++
@@ -1,9 +0,0 @@
-//Find character value from ascii
-#include<stdio.h>
-main()
-{
-	int no;
-	printf("Enter a ascii number:");
-	scanf("%d",&no);
-	printf("The character from ascii number is %c",no);
-}
