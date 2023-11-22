655a82517c1f7d152164b3064274ccda52cfc785
manavgathani <109840903+manavgathani@users.noreply.github.com>
2023-11-22 16:23:25 +0530

    Delete que12.c

--- que12.c
+++
@@ -1,9 +0,0 @@
-//Find ascii value of given character
-#include<stdio.h>
-main()
-{
-	char ch;
-	printf("Enter a character:");
-	scanf("%c",&ch);
-	printf("The ascii value of character is %d",ch);
-}
