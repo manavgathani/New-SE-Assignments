1ada19bb90139be86e588649eb0d97b817fcb540
manavgathani <109840903+manavgathani@users.noreply.github.com>
2023-11-22 16:26:56 +0530

    Delete que33.c

--- que33.c
+++
@@ -1,9 +0,0 @@
-//C Program to Read Integer and Print First Three Powers (N^1, N^2, N^3)
-#include<stdio.h>
-main()
-{
-	int n;
-	printf("Enter a number:");
-	scanf("%d",&n);
-	printf("The first three powers is %d %d %d",n,n*n,n*n*n);
-}
